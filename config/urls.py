8e16a93d9ad1c5250c64252981e1fcce70c98a36
Chris Mann <chris@lesgtrandsvoisins.com>
2024-05-06 08:55:07 +0200

    small changes after debugging

--- config/urls.py
+++ config/urls.py
@@ -16,4 +16,5 @@ urlpatterns += i18n_patterns(
     path("", include("content_manager.urls")),
     prefix_default_language=False,
 )
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
