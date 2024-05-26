8c121b58621156888c0db498e0f7ef4fa161ed2d
Chris Mann <chris@lesgtrandsvoisins.com>
2024-05-26 15:54:47 +0200

    wagtail_cefran_search

--- wagtail_cefran/urls.py
+++ wagtail_cefran/urls.py
@@ -13,7 +13,7 @@ from wagtail_cefran.views import SearchResultsView, TagsListView, TagView
 
 
 urlpatterns = [
-    path(_("search/"), SearchResultsView.as_view(), name="page_search"),
+    path(_("search/"), SearchResultsView.as_view(), name="wagtail_cefran_search"),
     path("tags/<str:tag>/", TagView.as_view(), name="global_tag"),
     path("tags/", TagsListView.as_view(), name="global_tags_list"),
     path("", include(wagtail_urls)),
