c405f52eb583be69d6167235045dc50560471ed1
Chris Mann <chris@lesgtrandsvoisins.com>
2024-06-07 07:21:00 +0200

    Wagtail Home Looking Good

--- wagtail_village_lesgrandsvoisins/models.py
+++ wagtail_village_lesgrandsvoisins/models.py
@@ -1,8 +1,40 @@
 from django.utils.translation import gettext_lazy as _
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField, StreamField
 
+from wagtail_village.blocks import STREAMFIELD_COMMON_BLOCKS
 from wagtail_village.models import ContentPage
 
 
 class WagtailVillageLesgrandsvoisinsHome(ContentPage):
+    title_rich = RichTextField(null=True, blank=True, verbose_name=_("Titre Homepage"))
+    description_rich = RichTextField(null=True, blank=True, verbose_name=_("Description Homepage"))
+
+    section_2_description_rich = RichTextField(null=True, blank=True, verbose_name=_("Description Section 2"))
+    section_2_body = StreamField(
+        STREAMFIELD_COMMON_BLOCKS,
+        blank=True,
+        use_json_field=True,
+    )
+
+    section_3_body = StreamField(
+        STREAMFIELD_COMMON_BLOCKS,
+        blank=True,
+        use_json_field=True,
+    )
+
+    content_panels = (
+        [
+            FieldPanel("title_rich"),
+            FieldPanel("description_rich"),
+        ]
+        + ContentPage.content_panels
+        + [
+            FieldPanel("section_2_description_rich"),
+            FieldPanel("section_2_body"),
+            FieldPanel("section_3_body"),
+        ]
+    )
+
     class Meta:
         verbose_name = _("Home Page Les Grands Voisins")
