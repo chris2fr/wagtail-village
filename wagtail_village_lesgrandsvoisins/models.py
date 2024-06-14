332c6b606e7e01f3c284f0afb6c68a4566f2a653
Chris Mann <chris@lesgtrandsvoisins.com>
2024-06-15 01:08:21 +0200

    Adjustments

--- wagtail_village_lesgrandsvoisins/models.py
+++ wagtail_village_lesgrandsvoisins/models.py
@@ -1,4 +1,5 @@
 from django.utils.translation import gettext_lazy as _
+from wagtail import blocks
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField, StreamField
 
@@ -6,7 +7,31 @@ from wagtail_village.blocks import STREAMFIELD_COMMON_BLOCKS
 from wagtail_village.models import ContentPage
 
 
+SECTION_CHOICES = [
+    ("layout01", _("Design 1")),
+    ("layout02", _("Design 2")),
+    ("layout03", _("Design 3")),
+    ("layout04", _("Design 4")),
+    ("layout05", _("Design 5")),
+    ("layout06", _("Design 6")),
+]
+
+
+class HomeSectionBlock(blocks.StructBlock):
+    layout = blocks.ChoiceBlock(
+        label=_("Design"),
+        choices=SECTION_CHOICES,
+        required=False,
+        default="layout02",
+        help_text=_("Choisir une mise en page variable"),
+    )
+    content = blocks.StreamBlock(
+        STREAMFIELD_COMMON_BLOCKS, blank=True, use_json_field=True, verbose_name=_("Heme Section Content")
+    )
+
+
 class WagtailVillageLesgrandsvoisinsHome(ContentPage):
+    sections = StreamField([("sections", HomeSectionBlock())], blank=True, use_json_field=True)
     section_1_body = StreamField(
         STREAMFIELD_COMMON_BLOCKS,
         blank=True,
@@ -26,17 +51,13 @@ class WagtailVillageLesgrandsvoisinsHome(ContentPage):
         use_json_field=True,
     )
 
-    content_panels = (
-        [
-            FieldPanel("section_1_body"),
-        ]
-        + ContentPage.content_panels
-        + [
-            FieldPanel("section_2_description_rich"),
-            FieldPanel("section_2_body"),
-            FieldPanel("section_3_body"),
-        ]
-    )
+    content_panels = [
+        FieldPanel("sections"),
+        FieldPanel("section_1_body"),
+        FieldPanel("section_2_description_rich"),
+        FieldPanel("section_2_body"),
+        FieldPanel("section_3_body"),
+    ] + ContentPage.content_panels
 
     class Meta:
         verbose_name = _("Home Page Les Grands Voisins")
