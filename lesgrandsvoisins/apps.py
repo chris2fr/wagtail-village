6ba36ace17646c2364f1d53304b3eb6d1467e14d
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-17 10:36:25 +0100

    Trying a new callback

---
+++ lesgrandsvoisins/apps.py
@@ -0,0 +1,16 @@
+from allauth.account.signals import user_signed_up
+from django.apps import AppConfig
+from django.contrib.auth.models import Group
+
+
+def user_signed_up_callback(sender, request, user, **kargs):
+    dashboard_user_group = Group.objects.get(name="dashboard")
+    user.groups.add(dashboard_user_group)
+
+
+class LesGrandsVoisinsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "lesgrandsvoisins"
+
+    def ready(self):
+        user_signed_up.connect(user_signed_up_callback)
--- lesgrandsvoisins/wsgi.py
+++ lesgrandsvoisins/wsgi.py
@@ -12,17 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-# from allauth.account.signals import user_signed_up
-# from django.contrib.auth.models import Group
-
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lesgrandsvoisins.settings.production")
 
 
 application = get_wsgi_application()
-
-
-# @receiver(user_signed_up)
-# def user_signed_up_callback(sender, request, user, **kargs):
-#     dashboard_user_group = Group.objects.get(name="dashboard")
-#     user.groups.add(dashboard_user_group)
