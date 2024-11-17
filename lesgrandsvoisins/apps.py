e62b4fb78ee95dd38bc03f06f104ef38da0a689b
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-17 10:46:38 +0100

    Trying something a little different for the hook / signal

--- lesgrandsvoisins/apps.py
+++ lesgrandsvoisins/apps.py
@@ -1,18 +1,17 @@
 from django.apps import AppConfig
+from django.contrib.auth.models import Group
 
 
-# from allauth.account.signals import user_signed_up
-# from django.contrib.auth.models import Group
-
-
-# def user_signed_up_callback(sender, request, user, **kargs):
-#     dashboard_user_group = Group.objects.get(name="dashboard")
-#     user.groups.add(dashboard_user_group)
+def user_signed_up_callback(sender, request, user, **kargs):
+    dashboard_user_group = Group.objects.get(name="dashboard")
+    user.groups.add(dashboard_user_group)
 
 
 class LesGrandsVoisinsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "lesgrandsvoisins"
 
-    # def ready(self):
-    #     user_signed_up.connect(user_signed_up_callback)
+    def ready(self):
+        from allauth.account.signals import user_signed_up
+
+        user_signed_up.connect(user_signed_up_callback)
