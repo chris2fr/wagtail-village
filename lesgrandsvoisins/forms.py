877a364913b9d9337754e18b5c356ecba10b5cad
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-17 18:30:46 +0100

    Improved signin form

--- lesgrandsvoisins/forms.py
+++ lesgrandsvoisins/forms.py
@@ -9,45 +9,62 @@ from django.utils.translation import gettext_lazy as _
 # declare a new model with a name "GeeksModel"
 class signup(forms.Form):
     # fields of the model
-    username = forms.CharField(
-        widget=forms.TextInput(attrs={"placeholder": _("nomdutilisateur"), "class": "required"}),
-        validators=[validators.validate_slug],
-        label=_("Pseudo d'utilisateur"),
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "placeholder": _("nomdutilisateur@example.com"),
+                "class": "required",
+                "onChange": "javascript:id_username.value=this.value.replace(/@.*/,'')",
+            }
+        ),
+        label=_("Email"),
         required=True,
-        label_suffix=" * ",
     )
     firstName = forms.CharField(
-        widget=forms.TextInput(attrs={"placeholder": _("Prénom")}),
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": _("Prénom"),
+                "onChange": "javascript:id_displayName.value=id_firstName.value + ' ' + id_lastName.value",
+            }
+        ),
         max_length=200,
         label=_("Prénom"),
         required=False,
-        label_suffix=" ",
     )
     lastName = forms.CharField(
-        widget=forms.TextInput(attrs={"placeholder": _("Nom de Famille"), "class": "required"}),
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": _("Nom de Famille"),
+                "class": "required",
+                "onChange": "javascript:id_displayName.value=id_firstName.value + ' ' + id_lastName.value",
+            }
+        ),
         max_length=200,
         label=_("Nom de famille"),
         required=True,
-        label_suffix="* ",
     )
-    email = forms.EmailField(
-        widget=forms.EmailInput(attrs={"placeholder": _("nomdutilisateur@example.com"), "class": "required"}),
-        label=_("Email"),
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": _("nomdutilisateur"), "class": "required"}),
+        validators=[validators.validate_slug],
+        label=_("identifiant"),
         required=True,
-        label_suffix=" * ",
     )
     telephoneNumber = forms.CharField(
         widget=forms.TextInput(attrs={"placeholder": _("+33 6 12 34 56 78")}),
         max_length=32,
         required=False,
         label=_("Numéro de téléphone"),
-        label_suffix="   ",
+    )
+    displayName = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": _("Oumar Xhi")}),
+        max_length=200,
+        label=_("Nom usuel affiché"),
+        required=False,
     )
     password = forms.CharField(
-        widget=forms.PasswordInput(attrs={"class": "required"}),
+        widget=forms.PasswordInput(attrs={"class": "required", "onClick": "javascript:peekAtPass(this)"}),
         label=_("Mot de passe"),
         required=True,
-        label_suffix=" * ",
     )
     captcha = CaptchaField()
 
