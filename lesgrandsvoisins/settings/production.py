6e1bc25861ec892e01e52125ee4e7c4edf4b13e5
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-17 09:39:16 +0100

    Adding user to a default group on creation

--- lesgrandsvoisins/settings/production.py
+++ lesgrandsvoisins/settings/production.py
@@ -1,4 +1,7 @@
+from allauth.account.signals import user_signed_up
+
 from .base import *
+
 
 DEBUG = False
 
@@ -6,3 +9,9 @@ try:
     from .local import *
 except ImportError:
     pass
+
+
+@receiver(user_signed_up)
+def user_signed_up_callback(sender, request, user, **kargs):
+    dashboard_user_group = Group.objects.get(name="dashboard")
+    user.groups.add(dashboard_user_group)
