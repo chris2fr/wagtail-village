79572b345416bda3e1a5b736c5c3b92a439e6855
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-17 09:56:15 +0100

    I think the settings come from lesgv in production

--- lesgv/settings/production.py
+++ lesgv/settings/production.py
@@ -1,3 +1,6 @@
+from allauth.account.signals import user_signed_up
+from django.contrib.auth.models import Group
+
 import lesgv.settings.secrets.lesecret
 
 from .base import *
@@ -62,3 +65,9 @@ try:
     from .local import *
 except ImportError:
     pass
+
+
+@receiver(user_signed_up)
+def user_signed_up_callback(sender, request, user, **kargs):
+    dashboard_user_group = Group.objects.get(name="dashboard")
+    user.groups.add(dashboard_user_group)
