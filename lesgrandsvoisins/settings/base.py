d5ff75b793deb51477eff541d2989554db3d2b4b
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-17 10:17:19 +0100

    django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet.

--- lesgrandsvoisins/settings/base.py
+++ lesgrandsvoisins/settings/base.py
@@ -17,6 +17,9 @@ import dj_database_url  # Pour un syntaxe différent de base de données
 from dotenv import load_dotenv  # Pour les variables d'.env
 
 
+# from allauth.account.signals import user_signed_up
+# from django.contrib.auth.models import Group
+
 # Prendre les variables d'environnement
 load_dotenv()
 
@@ -446,3 +449,9 @@ ACCOUNT_DEFAULT_HTTP_PROTOCOL = "https"
 # # ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
 # ACCOUNT_USERNAME_BLACKLIST = ["admin", "god"]
 # # ACCOUNT_USERNAME_MIN_LENGTH = 2
+
+
+# @receiver(user_signed_up)
+# def user_signed_up_callback(sender, request, user, **kargs):
+#     dashboard_user_group = Group.objects.get(name="dashboard")
+#     user.groups.add(dashboard_user_group)
--- lesgrandsvoisins/settings/production.py
+++ lesgrandsvoisins/settings/production.py
@@ -1,6 +1,3 @@
-from allauth.account.signals import user_signed_up
-from django.contrib.auth.models import Group
-
 from .base import *
 
 
@@ -10,9 +7,3 @@ try:
     from .local import *
 except ImportError:
     pass
-
-
-@receiver(user_signed_up)
-def user_signed_up_callback(sender, request, user, **kargs):
-    dashboard_user_group = Group.objects.get(name="dashboard")
-    user.groups.add(dashboard_user_group)
