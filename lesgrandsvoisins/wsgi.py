ce0c7be5da425fc67e3e24e15971aad0dd987732
Chris Mann <chris@lesgtrandsvoisins.com>
2024-11-17 10:25:01 +0100

    reciever placement

--- lesgrandsvoisins/wsgi.py
+++ lesgrandsvoisins/wsgi.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
-from allauth.account.signals import user_signed_up
-from django.contrib.auth.models import Group
 from django.core.wsgi import get_wsgi_application
+
+
+# from allauth.account.signals import user_signed_up
+# from django.contrib.auth.models import Group
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lesgrandsvoisins.settings.production")
@@ -20,7 +22,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lesgrandsvoisins.settings.produ
 application = get_wsgi_application()
 
 
-@receiver(user_signed_up)
-def user_signed_up_callback(sender, request, user, **kargs):
-    dashboard_user_group = Group.objects.get(name="dashboard")
-    user.groups.add(dashboard_user_group)
+# @receiver(user_signed_up)
+# def user_signed_up_callback(sender, request, user, **kargs):
+#     dashboard_user_group = Group.objects.get(name="dashboard")
+#     user.groups.add(dashboard_user_group)
